c30974dbbc4a0d4e7b7112614fb6d90ed5c98190
Mayra Estrella <93684336+mayra84@users.noreply.github>
2022-02-24 17:20:20 -0600

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/register" element={ <Register /> } />

e2087da97c5564f80968f21b13c8cebdb888eeb7
Mayra Estrella <93684336+mayra84@users.noreply.github>
2022-02-24 11:52:38 -0600

    register form for frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+// import Features from '../Features'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+    <div>Register to discover the Pawsibilities.</div>
+    {/* <Features /> */}
+    
+    </div>
   )
 }
 

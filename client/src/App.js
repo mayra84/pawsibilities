d0425af69c62b5d88e0e3733e660c3b80708ba72
zaria <mikeburrows8114@gmail.com>
2022-02-27 11:51:49 -0500

    google-map-react installed, got a sample map to display (i need to set up billing on google cloud acc for it to work)

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Map from './pages/Map'
 
 // import Carousel from './components/Carousel';
 
@@ -25,6 +26,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
+<Route path="/map" element={<Map/>} />
 
       </Routes>
     </div>

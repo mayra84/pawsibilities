e41af5894849995258d7323edbbc611501981cbe
zaria <mikeburrows8114@gmail.com>
2022-03-04 15:54:13 -0500

    added needed info to start on rendering google map

--- client/src/components/DogPark.js
+++ client/src/components/DogPark.js
@@ -1,29 +1,60 @@
-import { useSelector } from 'react-redux'
-import React from "react";
+import { connect, useSelector } from 'react-redux'
+import React, {useEffect} from "react";
 import GoogleMapReact from 'google-map-react';
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
 
-export default function DogPark () {
+function DogPark ({currentUser}) {
+  // const [currentWeather, setCurrentWeather] = useState(null);
+  console.log ("this is the ", currentUser)
+
+//   useEffect (() => {
+//     fetchWeather(currentUser)
+// }, [])
+
+
+// const fetchWeather = async (currentUser) => {
+
+//   await fetch(`http://api.weatherapi.com/v1/current.json?key=ca26e518dd18404c95f191858222702&q=${currentUser.zipcode}&`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//           var weather = data
+//           console.log(weather)
+//           console.log(weather.current.condition.text)
+//           setCurrentWeather(weather)
+//       })
+// }
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627
+      lat: 34.05,
+      lng: -84.38
     },
     zoom: 11
   };
 
+
+  // axios.get('/api/v1/users')
+  //   .then(res => {
+  //       console.log(res.data[0].zipcode)
+  //       fetchWeather(res.data)
+  //   })
+
+
+
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '50vh', width: '50%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyCdA5mxV4NJuOjewdQpY7-fBqxTbPqUbR4" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
+          lat={34.05}
+          lng={-84.38}
           text="My Marker"
         />
       </GoogleMapReact>
@@ -31,9 +62,9 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
   );
 }
 
-//ADD VETS
-
-// Geocode.fromAddress(this.state.zipCode).then(
+// Geocode.setLanguage('en');
+// Geocode.setApiKey("AIzaSyCdA5mxV4NJuOjewdQpY7-fBqxTbPqUbR4" );
+// Geocode.fromAddress(currentUser.zipCode).then(
 //     response => {
 //         const { lat, lng } = response.results[0].geometry.location;
 //         const { google } = this.props;
@@ -42,6 +73,16 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 //         var request = {
 //             location: startPoint,
 //             radius: '50000',
-//             query: ['dog park'],
+//             query: ['dog park'], //vet, adoption center
 //             fields: ['name', 'geometry', 'formatted_address', 'formatted_phone_number', 'website'],
 //         };
+
+const mapStateToProps = (state) => {
+  console.log("Dog park componenet", state)
+  const {currentUser} = state.user 
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps) (DogPark)

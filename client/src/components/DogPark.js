fa1ac344cb759a4b54521391954f6ff9953528c0
zaria <mikeburrows8114@gmail.com>
2022-03-04 14:26:46 -0500

    starting in google maps api

--- client/src/components/DogPark.js
+++ client/src/components/DogPark.js
@@ -1,9 +1,32 @@
-import { useSelector } from 'react-redux'
-import React from "react";
+import { connect, useSelector } from 'react-redux'
+import React, {useEffect} from "react";
 import GoogleMapReact from 'google-map-react';
+import Weather from './Weather';
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
 
-export default function DogPark () {
+function DogPark ({currentUser}) {
+  // const [currentWeather, setCurrentWeather] = useState(null);
+  console.log ("this is the ", currentUser)
+
+//   useEffect (() => {
+//     fetchWeather(currentUser)
+// }, [])
+
+
+// const fetchWeather = async (currentUser) => {
+
+//   await fetch(`http://api.weatherapi.com/v1/current.json?key=ca26e518dd18404c95f191858222702&q=${currentUser.zipcode}&`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//           var weather = data
+//           console.log(weather)
+//           console.log(weather.current.condition.text)
+//           setCurrentWeather(weather)
+//       })
+// }
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
   const defaultProps = {
     center: {
@@ -12,6 +35,14 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
     },
     zoom: 11
   };
+
+
+  // axios.get('/api/v1/users')
+  //   .then(res => {
+  //       console.log(res.data[0].zipcode)
+  //       fetchWeather(res.data)
+  //   })
+
 
 
   return (
@@ -32,7 +63,9 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
   );
 }
 
-// Geocode.fromAddress(this.state.zipCode).then(
+// Geocode.setLanguage('en');
+// Geocode.setApiKey("AIzaSyCdA5mxV4NJuOjewdQpY7-fBqxTbPqUbR4" );
+// Geocode.fromAddress(currentUser.zipCode).then(
 //     response => {
 //         const { lat, lng } = response.results[0].geometry.location;
 //         const { google } = this.props;
@@ -41,6 +74,16 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 //         var request = {
 //             location: startPoint,
 //             radius: '50000',
-//             query: ['dog park'],
+//             query: ['dog park'], //vet, adoption center
 //             fields: ['name', 'geometry', 'formatted_address', 'formatted_phone_number', 'website'],
 //         };
+
+const mapStateToProps = (state) => {
+  console.log("Weather componenet", state)
+  const {currentUser} = state.user 
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps) (DogPark)
